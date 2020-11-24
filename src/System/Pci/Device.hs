f40a6c03faf2c4d883368a5581b24e945fb9e1bc
dopamane <dwc1295@gmail.com>
2020-11-24 05:05:19 -0500

    draft read/write functions

--- src/System/Pci/Device.hs
+++ src/System/Pci/Device.hs
@@ -5,11 +5,18 @@ module System.Pci.Device
   , readByte
   , readWord
   , readLong
+  , readBlock
+  , readVpd
+  , writeByte
+  , writeWord
+  , writeLong
+  , writeBlock
   ) where
 
 import Data.Word
 import Foreign.C.Types
 import Foreign.Ptr
+import Foreign.Marshal.Array
 import Foreign.Storable
 import System.Pci.Access
 import Bindings.Libpci.Pci
@@ -24,9 +31,9 @@ mkDevice :: Access -> Ptr C'pci_dev -> IO Device
 mkDevice acc pciDevPtr = do
   pciDev <- peek pciDevPtr
   return $ Device
-    { access      = acc
-    , devicePtr   = pciDevPtr
-    , next        = c'pci_dev'next pciDev
+    { access    = acc
+    , devicePtr = pciDevPtr
+    , next      = c'pci_dev'next pciDev
     }
 
 getDevices :: Access -> IO [Device]
@@ -51,3 +58,47 @@ readWord device = fmap fromIntegral . c'pci_read_word (devicePtr device)
 readLong :: Device -> CInt -> IO Word32
 readLong device = fmap fromIntegral . c'pci_read_long (devicePtr device)
 
+readBlock :: Device -> CInt -> Int -> IO (Either String [Word8])
+readBlock device pos len = allocaArray len $ \buf -> do
+  status <- c'pci_read_block (devicePtr device) pos buf $ fromIntegral len
+  if status == 1
+    then Right . fmap fromIntegral <$> peekArray len buf
+    else return $ Left "readBlock error"
+
+readVpd :: Device -> CInt -> Int -> IO (Either String [Word8])
+readVpd device pos len = allocaArray len $ \buf -> do
+  status <- c'pci_read_vpd (devicePtr device) pos buf $ fromIntegral len
+  if status == 1
+    then Right . fmap fromIntegral <$> peekArray len buf
+    else return $ Left "readVpd error"
+
+writeByte :: Device -> CInt -> Word8 -> IO (Maybe String)
+writeByte device pos byte = do
+  status <- c'pci_write_byte (devicePtr device) pos $ fromIntegral byte
+  return $ if status == 1
+    then Nothing
+    else Just "writeByte error"
+
+writeWord :: Device -> CInt -> Word16 -> IO (Maybe String)
+writeWord device pos word = do
+  status <- c'pci_write_word (devicePtr device) pos $ fromIntegral word
+  return $ if status == 1
+    then Nothing
+    else Just "writeWord error"
+
+writeLong :: Device -> CInt -> Word32 -> IO (Maybe String)
+writeLong device pos long = do
+  status <- c'pci_write_long (devicePtr device) pos $ fromIntegral long
+  return $ if status == 1
+    then Nothing
+    else Just "writeLong error"
+
+writeBlock :: Device -> CInt -> [Word8] -> IO (Maybe String)
+writeBlock device pos block = allocaArray len $ \bufPtr -> do
+  pokeArray bufPtr $ fmap fromIntegral block
+  status <- c'pci_write_block (devicePtr device) pos bufPtr $ fromIntegral len
+  return $ if status == 1
+    then Nothing
+    else Just "writeBlock error"
+  where
+    len = length block
